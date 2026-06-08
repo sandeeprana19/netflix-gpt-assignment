65b4e7d08e06c266e3c3a6590ee4fde2b2ae9546
Sandeep Rana <62502757+sandeeprana19@users.noreply.github.com>
2026-06-08 08:09:11 +0530

    Fix: Here video gradient.

--- src/utils/customHooks/useMovieVideos.jsx
+++ src/utils/customHooks/useMovieVideos.jsx
@@ -7,7 +7,6 @@ const useMovieVideos = (movieId) => {
   const dispatch = useDispatch();
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const getMovieVideos = async () => {
     const data = await fetch(
       `https://api.themoviedb.org/3/movie/${movieId}/videos`,
